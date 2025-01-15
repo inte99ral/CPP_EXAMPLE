b9b840367d4f8cb024ce55bd86df9f066db07b73
integral <inte99ral@gmail.com>
2025-01-15 15:29:06 +0900

    Fix by convention
    example 를 컨벤션에 맞게 수정했습니다.

--- CPP_ASIO/dev/Main.cpp
+++ CPP_ASIO/dev/Main.cpp
@@ -1,13 +1,20 @@
-#include <iostream>
-#include <string>
+#include <bits/stdc++.h>
 #include <boost/asio.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
 
 using namespace std;
-using boost::asio::ip::tcp;
+using namespace boost::asio;
+using namespace boost::posix_time;
 
 int main(int argc, char* argv[]) {
     // 코드 내용
     cout << "5 sec Timer" << endl;
+
+    io_context io;
+    deadline_timer t(io, seconds(5));
+
+    t.wait();
+    cout << "5 sec time over" << endl;
+
     return 0;
 }
-
